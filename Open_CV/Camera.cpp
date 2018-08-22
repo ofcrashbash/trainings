46ffe02a067406c1d97cee8932fbd88d7d9e0fe8
Oleg Kmechak <ofcrashbash@gmail.com>
2018-08-22 19:47:22 +0300

    Experimenting with filters in OpenCV

--- Open_CV/Camera.cpp
+++ Open_CV/Camera.cpp
@@ -4,42 +4,50 @@
 using namespace cv;
 using namespace std;
 
+enum class tFilters {erode, dilate, gaussian, laplacian, invert, eq_hist, LAST_EL};
+// Special behavior for ++Colors
+tFilters& operator++(tFilters &f) {
+	using IntType = typename std::underlying_type<tFilters>::type;
+		f = static_cast<tFilters>(static_cast<IntType>(f) + 1);
+	if (f == tFilters::LAST_EL)
+		f = static_cast<tFilters>(0);
+	return f;
+}
+
 int main(int argc, char* argv[])
 {
-	VideoCapture cap(1);
-	if (!cap.isOpened())
-	{
+	VideoCapture cap_back(1);
+	if (!cap_back.isOpened()){
+		cout << "Cannot open camera" << endl;
+		cin.get();
+		return -1;
+	}
+	
+	VideoCapture cap_front(0);
+	if (!cap_front.isOpened()){
 		cout << "Cannot open camera" << endl;
 		cin.get();
 		return -1;
 	}
 
-	double dWidth = cap.get(CAP_PROP_FRAME_WIDTH);
-	double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
 
-	int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
-	int frame_height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
-	Size frame_size(frame_width, frame_width);
+	string window_name_1 = "camera feed";
+	namedWindow(window_name_1);
 
-	cout << dWidth << "x" << dHeight << "?=" << frame_width << "x" << frame_height << endl;
-
-	int frames_per_sec = 5;
-	auto codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
-	VideoWriter oVideoWriter("out.avi", codec, frames_per_sec, frame_size, true);
-	if (!oVideoWriter.isOpened())
-	{
-		cout << "Cannot save the video to a file" << endl;
-		cin.get();
-		return -1;
-	}
-
-	string window_name = "camera feed";
-	namedWindow(window_name);
-
+	tFilters cur_filter = tFilters::invert;
 	while (true)
 	{
-		Mat frame, bgr_frame;
-		bool bSuccess = cap.read(frame);
+		Mat frame_front, frame_back, cur_frame;
+
+		bool bSuccess = cap_front.read(frame_front);
+		if (!bSuccess)
+		{
+			cout << "Failed to get frame" << endl;
+			cin.get();
+			break;
+		}
+
+		bSuccess = cap_back.read(frame_back);
 		if (!bSuccess)
 		{
 			cout << "Failed to get frame" << endl;
@@ -53,23 +61,58 @@ int main(int argc, char* argv[])
 			3 - Draw shapes
 		etc..
 		*/
-		//color conversion
-		//cvtColor(frame, bgr_frame, COLOR_RGB2BGR);
 
-		oVideoWriter.write(bgr_frame);
+		cur_frame = frame_back;
 
+		vector<Mat> vec_channels;
+		switch (cur_filter)
+		{
+		case tFilters::eq_hist:
+			cvtColor(cur_frame, cur_frame, COLOR_BGR2YCrCb);
+			split(cur_frame, vec_channels);
+			equalizeHist(vec_channels[0], vec_channels[0]);
+			equalizeHist(vec_channels[1], vec_channels[1]);
+			equalizeHist(vec_channels[2], vec_channels[2]);
+			merge(vec_channels, cur_frame);
+			cvtColor(cur_frame, cur_frame, COLOR_YCrCb2BGR);
+			break;
+		case tFilters::erode:
+			erode(cur_frame, cur_frame, getStructuringElement(MORPH_RECT, Size(5, 5)));
+			break;
+		case tFilters::gaussian:
+			blur(cur_frame, cur_frame, Size(3,3));
+			break;
+		case tFilters::invert:
+			cur_frame.convertTo(cur_frame, -1, -1, 255);
+			break;
+		case tFilters::dilate:
+			dilate(cur_frame, cur_frame, getStructuringElement(MORPH_RECT, Size(5, 5)), Point(-1,-1), 2);
+			break;
+		case tFilters::laplacian:
+			Laplacian(cur_frame, cur_frame, 0);
+			break;
+		default:
+			cout << "No such filter" << endl;
+			break;
+		}
 
-		imshow(window_name, frame);
+		imshow(window_name_1, cur_frame);
 
-		if (waitKey(10) == 27)
+		auto key = waitKey(10);
+		cout << "Pressed key: " << key << endl;
+		if (key == 27)
 		{
 			cout << "Esc is presse by user" << endl;
 			break;
 		}
+		else if (key == 32)
+		{
+			++cur_filter;
+		}
 	}
 
-	cap.release();
-	oVideoWriter.release();
+	cap_back.release();
+	cap_front.release();
 	destroyAllWindows();
 	return 0;
 }
