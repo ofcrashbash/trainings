8ac8d1204219b181b286e2913d3ab5be3f10af78
Oleg Kmechak <ofcrashbash@gmail.com>
2018-09-03 21:44:58 +0300

    refactoring and added android projects

---
+++ STL_Book/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits>
+#include "stl_code.h"
+
+int main(int argc, char ** argv)
+{
+	
+	stl_book::first::decomposition();
+	stl_book::first::name_visibility();
+	stl_book::first::constrexpr_if();
+	stl_book::first::variable_arguments_count();
+
+	stl_book::second::erase_remove_vector();
+	stl_book::second::sorted_vector();
+	stl_book::second::insert_into_map();
+	stl_book::second::unordered_map();
+	stl_book::second::unique_set();
+	stl_book::second::working_with_stack();
+	stl_book::second::map_histogram();
+	stl_book::second::multimap_test();
+	stl_book::second::priority_quene_test();
+
+
+	return 0;
+}
---
+++ STL_Book/stl_code.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <list>
+#include <array>
+#include <chrono>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <set>
+#include <cassert>
+#include <unordered_map>
+#include <stack>
+#include <sstream>
+#include <stdexcept>
+#include <cmath>
+#include <iomanip>
+#include <queue>
+
+int vetor_test();
+int map_test();
+int list_test();
+int array_test();
+namespace stl_book {
+	namespace first {
+		void decomposition(void);
+		void name_visibility(void);
+		void curvly_bracket_initialization(void);
+		void automatic_template_type(void);
+		void constrexpr_if(void);
+		void variable_arguments_count(void);
+	}
+	namespace second {
+		void deque(void);
+		void erase_remove_vector(void);
+		void sorted_vector(void);
+		void insert_into_map(void);
+		void unordered_map(void);
+		void unique_set(void);
+		void working_with_stack(void);
+		void map_histogram(void);
+		void multimap_test(void);
+		void priority_quene_test(void);
+	}
+}
