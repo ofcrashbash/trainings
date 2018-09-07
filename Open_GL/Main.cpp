502e1abdf7d20cb100d2d4ab034434f7945c1ef6
Oleg Kmechak <ofcrashbash@gmail.com>
2018-09-08 00:24:40 +0200

    refactoring of OpenGL, glew and freeglut solution moved to separate location from main

--- Open_GL/Main.cpp
+++ Open_GL/Main.cpp
@@ -1,34 +1,7 @@
-#include <GL/glew.h>
-#include <GL/freeglut.h>
-#include <iostream>
-
-using namespace std;
-
-void renderScene(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(.1, 0.5, 1.0, 1.0);//clear red
-	glutSwapBuffers();
-}
-
+#include "glew_and_freeglut.h"
 int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(500, 500);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("OpenGL First Window");
+	start_window(argc, argv);
 
-
-	glewInit();
-	if (glewIsSupported("GL_VERSION_4_5"))
-		cout << " GLEW Version is 4.5" << endl;
-	else
-		cout << "GLEW 4.5 not supported" << endl;
-
-	glEnable(GL_DEPTH_TEST);
-	// register callbacks
-	glutDisplayFunc(renderScene);//this func is pretty important!
-	glutMainLoop();
 	return 0;
 }
---
+++ Open_GL/glew_and_freeglut.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+
+int start_window(int argc, char *argv[]);
