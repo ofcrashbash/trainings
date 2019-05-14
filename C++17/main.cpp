8459a25e932cf05d21a0bab14fc31c8deb47758e
Oleg Kmechak <ofcrashbash@gmail.com>
2019-05-14 19:45:13 +0200

    Basic C++17 LANGUAGE FEATURES TEST

--- C++17/main.cpp
+++ C++17/main.cpp
@@ -1,5 +1,8 @@
 #include "c++.h"
 #include "basicclasses.h"
+#include "friendshipandinheritance.h"
+#include "typecasting.h"
+#include "Exceptions.h"
 
 int main(int argc, char* argv[])
 {
@@ -13,12 +16,34 @@ int main(int argc, char* argv[])
 	memory_allocation();
 	anonimous_union_and_structure();
 
-	//Basic Classes Syntaxes
+	//Basic Classes Syntaxes 
 	test_FirstOneClass();
 	initializer_list_test();
 	overloaded_operators();
 	class_static_members();
 
+	const_class_test();
+	template_test();
+	special_members_test();
+
+	//friendship and inheritance
+	cout << "firnedhsip and relateions" << endl;
+	auto a = SomeClass{};
+	friendfunction(a);
+	friendclass();
+	inheritance_test();
+	constructor_calls();
+	polymorfism_test();
+
+	//Types
+	typecasting();
+	dynamic_cast_test();
+	static_cast_test();
+	const_cast_test();
+	typeid_test();
+
+	//exceptions
+	simple_except();
 
 	return 0;
 }
