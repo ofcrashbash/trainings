789f59f54aed8c4f9f2486a7525c00d4e38cc911
Oleg Kmechak <ofcrashbash@gmail.com>
2020-02-26 17:54:12 +0100

    [MAJOR] solve laplacea on different grids and renumberings

--- dealii/step-2.hpp
+++ dealii/step-2.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_out.h>
-#include <deal.II/grid/manifold_lib.h>
+#include "step-1.hpp"
 
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
@@ -16,13 +11,6 @@
 
 #include <deal.II/lac/sparse_matrix.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
-
-#include <iostream>
-#include <fstream>
-#include <cmath>
-
-using namespace dealii;
-using namespace std; 
 
 SparsityPattern generate_sparsity_pattern(DoFHandler<2> &dof_handler);
 
@@ -37,18 +25,9 @@ enum renumberings
     subdomain_wise
 };
 
-template <int dim = 2>
-void distribute_dofs(
-    Triangulation<dim> & triangulation, 
-    renumberings renumbering_type = none, 
-    int fe_degree = 1, 
-    string file_name = "sparsity_pattern.svg")
+template <int dim = 2, int spacedim = dim>
+void renumber(DoFHandler<dim, spacedim> & dof_handler, renumberings renumbering_type)
 {
-    const FE_Q<dim> finite_element(fe_degree);
-    DoFHandler<dim> dof_handler(triangulation);
-    dof_handler.distribute_dofs(finite_element);
-    cout << dof_handler;
-
     switch(renumbering_type)
     {
         case none:
@@ -60,7 +39,7 @@ void distribute_dofs(
             DoFRenumbering::block_wise(dof_handler);
             break;
         case clockwise_dg:
-            DoFRenumbering::clockwise_dg(dof_handler, Point<2>(0, 0));
+            DoFRenumbering::clockwise_dg(dof_handler, Point<dim>(0, 0));
             break;
         case hierarchical:
             DoFRenumbering::hierarchical(dof_handler);
@@ -72,6 +51,21 @@ void distribute_dofs(
             DoFRenumbering::subdomain_wise(dof_handler);
             break;
     }
+}
+
+template <int dim = 2, int spacedim = dim>
+void distribute_dofs(
+    Triangulation<dim> & triangulation, 
+    renumberings renumbering_type = none, 
+    int fe_degree = 1, 
+    string file_name = "sparsity_pattern.svg")
+{
+    const FE_Q<dim, spacedim> finite_element(fe_degree);
+    DoFHandler<dim, spacedim> dof_handler(triangulation);
+    dof_handler.distribute_dofs(finite_element);
+    cout << dof_handler;
+
+    renumber(dof_handler, renumbering_type);
 
     auto sparsity_pattern = generate_sparsity_pattern(dof_handler);
     dof_handler.clear();
@@ -79,4 +73,14 @@ void distribute_dofs(
     ofstream out(file_name);
     sparsity_pattern.print_svg(out);
     out.close();
+}
+
+template <int dim = 2>
+ostream& operator<<(ostream &out, const DoFHandler<dim>& dof_handler)
+{
+    cout << "DoF Handler info" << endl;
+    cout << "number of dofs: " << dof_handler.n_dofs() << endl;
+    cout << "number of boundary dofs: " << dof_handler.n_boundary_dofs() << endl;
+    cout << "max connections: " << dof_handler.max_couplings_between_dofs() << endl;
+    return out;
 }
