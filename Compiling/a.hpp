557948d7b666a4b9a9f95061f6298a0058a2604a
Oleg Kmechak <ofcrashbash@gmail.com>
2020-03-01 18:57:21 +0100

    new compiling training project add

---
+++ Compiling/a.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+int some_function(string key);
---
+++ Compiling/b.cpp
@@ -0,0 +1,14 @@
+#include "a.hpp"
+#include "b.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+#define RETURN return 0
+
+int main() {
+    cout << "Hello, world!" << endl;
+    cout << some_function("one") << endl;
+    return 0;
+}
