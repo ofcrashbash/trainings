d21cff652e3a020e192a45744fffa541a234bc21
Oleg Kmechak <ofcrashbash@gmail.com>
2020-03-05 13:28:45 +0100

    main inprove error handling

--- dealii/main.cpp
+++ dealii/main.cpp
@@ -6,9 +6,22 @@
 
 int main(void)
 {
-    step_1_main();//WTF why when I comment out this, error with std11 occur
-    //step_2_main();
-    step_3_main();
-    
+    try
+    {
+        step_1_main();//WTF why when I comment out this, error with std11 occur
+        //step_2_main();
+        step_3_main();
+    }
+    catch(const exception &exc)
+    {
+        cerr << endl <<"Exception on processing: " << endl
+            << exc.what() << endl << endl;
+    }
+    catch(...)
+    {
+        cerr << "Unknown exception" << endl;
+        throw;
+    }
+        
     return 0;
 }
