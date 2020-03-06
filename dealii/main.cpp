a21d5120b81692868848bca96725bff220c3c9e6
Oleg Kmechak <ofcrashbash@gmail.com>
2020-03-06 20:09:19 +0100

    add test of adaptive refinement mesh

--- dealii/main.cpp
+++ dealii/main.cpp
@@ -1,6 +1,7 @@
 #include "step-1.hpp"
 #include "step-2.hpp"
 #include "step-3.hpp"
+#include "step-6.hpp"
 
 #include <map>
 
@@ -11,6 +12,7 @@ int main(void)
         step_1_main();//WTF why when I comment out this, error with std11 occur
         //step_2_main();
         step_3::run();
+        step_6::run();
     }
     catch(const exception &exc)
     {
