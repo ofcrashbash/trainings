db93063872b2585db3b9bee6e85573976d3aa320
Oleg Kmechak <ofcrashbash@gmail.com>
2020-03-18 17:32:52 +0100

    recover implementation

--- prometeus_cs/recover.c
+++ prometeus_cs/recover.c
@@ -4,18 +4,42 @@
 #include <stdlib.h>
 #include "bmp.h"
 
+//jpeg
+
+typedef struct
+{
+    BYTE first;
+    BYTE second;
+    BYTE third;
+    BYTE forth;
+} __attribute__((__packed__))
+JPEGHEADERKEY;
+
+int is_jpeg_header(JPEGHEADERKEY *header)
+{
+    if( header->first == 0xff && 
+        header->second == 0xd8 && 
+        header->third == 0xff && 
+        (header->forth & 0xf0) == 0xe0)
+
+        return 1;
+
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
     // ensure proper usage
-    if (argc != 3)
+    if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
+        fprintf(stderr, "Usage: %s raw_file_name\n", argv[0]);
         return 1;
     }
 
     // remember filenames
     char *infile = argv[1];
-    char *outfile = argv[2];
+    char *outfile = "image";
 
     // open input file
     FILE *inptr = fopen(infile, "r");
@@ -25,73 +49,28 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    // open output file
-    FILE *outptr = fopen(outfile, "w");
-    if (outptr == NULL)
-    {
-        fclose(inptr);
-        fprintf(stderr, "Could not create %s.\n", outfile);
-        return 3;
-    }
-
-    // read infile's BITMAPFILEHEADER
-    BITMAPFILEHEADER bf;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
-
-    // read infile's BITMAPINFOHEADER
-    BITMAPINFOHEADER bi;
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
-
-    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
-        bi.biBitCount != 24 || bi.biCompression != 0)
-    {
-        fclose(outptr);
-        fclose(inptr);
-        fprintf(stderr, "Unsupported file format.\n");
-        return 4;
-    }
-
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
-    // determine padding for scanlines
-    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
-    // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
-    {
-        // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
+    unsigned jpeg_counter = 0;
+    int ch = getc(inptr); 
+    while (ch != EOF)  
+    { 
+        /* display contents of file on screen */ 
+        JPEGHEADERKEY data;
+        fread(&data, sizeof(JPEGHEADERKEY), 1, inptr);
+        if(is_jpeg_header(&data))
         {
-            // temporary storage
-            RGBTRIPLE triple;
-
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            printf("Got: %u!\n", jpeg_counter);
+            jpeg_counter++;
         }
+        
 
-        // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
+        ch = getc(inptr); 
+    } 
 
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
-        }
-    }
+    // skip over padding, if any
+    fseek(inptr, sizeof(JPEGHEADERKEY), SEEK_CUR);
 
     // close infile
     fclose(inptr);
-
-    // close outfile
-    fclose(outptr);
 
     // success
     return 0;
